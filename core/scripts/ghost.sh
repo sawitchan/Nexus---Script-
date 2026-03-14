ff29372815bfe2ef8e1158fb2c95a2cb1096b2af
tapscan <visi4ura@gmail.com>
2026-03-15 06:33:32 +0700

    V2.1: System Keep Alive & Auto-Maintenance

--- core/scripts/ghost.sh
+++ core/scripts/ghost.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 while true; do
+    # Pembersihan 5 Menit
     rm -rf ~/.cache/*
     pkg clean
     sleep 120
+    # Sync GitHub 7 Menit
     cd ~/Nexus-Omni
-    git pull origin main # Ambil update terbaru biar gak bentrok
     git add .
-    git commit -m "V30.0: Maintenance & Secure Sync"
+    git commit -m "V2.1: System Keep Alive & Auto-Maintenance"
     git push origin main
     sleep 300
 done
