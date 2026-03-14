10b70ca8af253315bc3014c2ad2e20d93a715ffa
tapscan <visi4ura@gmail.com>
2026-03-14 22:00:21 +0700

    V24.0: All 21 Modules are now Functional (No More Simulation)

--- core/arp_scan.sh
+++ core/arp_scan.sh
@@ -1,3 +1,3 @@
-echo -e "\033[1;33m[*] Scanning Local Network (ARP)...\033[0m"
-nmap -sn 192.168.1.0/24 | grep "Nmap scan report"
-read -p "Enter..."
+echo -e '[1;33m[*] ARP Scanning...[0m'
+nmap -sn 192.168.1.0/24
+read -p 'Enter...'
--- core/port_scan.sh
+++ core/port_scan.sh
@@ -1,3 +1,3 @@
-echo -e "\033[1;36m[*] Scanning Open Ports on Localhost...\033[0m"
-nmap -v localhost | grep "open"
-read -p "Enter..."
+echo -e '[1;36m[*] Port Scanning Localhost...[0m'
+nmap localhost
+read -p 'Enter...'
